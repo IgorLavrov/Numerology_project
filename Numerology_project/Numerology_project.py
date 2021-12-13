230a9492dbc98663a3549a73b968d529e9f569f0
Lavrovi <igor.lavrov90@hotmail.co.uk>
2021-12-14 00:10:27 +0200

    сommit

--- Numerology_project/Numerology_project.py
+++ Numerology_project/Numerology_project.py
@@ -11,26 +11,28 @@ root.title('Тайна Вашего имени скрыта здесь!')
    
 number1 = tk.StringVar() 
 res=tk.IntVar()
-  
-labelNum1 = tk.Label(root, text="Введите ваше имя:").grid(row=1, column=0)   
+
+labelNum2 = tk.Label(root, text="Тайна Вашего имени:").grid(row=1, column=0)  
+labelNum1 = tk.Label(root, text="Введите ваше имя на русском:").grid(row=2, column=0)   
   
 labelResult = tk.Label(root)
 
   
 labelResult.grid(row=7, column=2,sticky='w',pady=2)  
  
-entryNum1 = tk.Entry(root, textvariable=number1).grid(row=1, column=2)  
+entryNum1 = tk.Entry(root, textvariable=number1).grid(row=2, column=2)  
 
-entryNum2 = tk.Entry(root, textvariable=res).grid(row=25, column=2) 
+entryNum2 = tk.Entry(root, textvariable=res).grid(row=22, column=2) 
   
  
 call_result = partial(name,labelResult,number1)
 aken2=partial(aken,res)
   
-buttonCal = tk.Button(root, text="Вычислить", command=call_result).grid(row=3, column=0)  
+buttonCal = tk.Button(root, text="Вычислить", command=call_result).grid(row=4, column=0)  
 
-buttonCal2 = tk.Button(root, text=" Узнать расшифровку своего имени",command=aken2).grid(row=20, column=0)  
-labelNum2 = tk.Label(root, text="Введите число:").grid(row=21, column=0)   
+buttonCal2 = tk.Button(root, text=" Узнать расшифровку своего имени",command=aken2).grid(row=25, column=0)  
+labelNum2 = tk.Label(root, text="Тайна вашего числа:").grid(row=20, column=0)
+labelNum3 = tk.Label(root, text="Введите число:").grid(row=21, column=0)   
 
   
 root.mainloop() 
